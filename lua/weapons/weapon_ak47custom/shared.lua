32b8d21f99bc0e1b03635b60ff22643419c37769
Arbenit <arbenit.ca@hotmail.ca>
2016-01-03 17:16:34 -0500

    Update shared.lua

--- lua/weapons/weapon_ak47custom/shared.lua
+++ lua/weapons/weapon_ak47custom/shared.lua
@@ -37,7 +37,7 @@ SWEP.Primary.NumShots = 1
 SWEP.Primary.Cone = 0.002
 SWEP.Primary.ClipSize = 30
 SWEP.Primary.Delay = 0.08
-SWEP.Primary.DefaultClip = 31
+SWEP.Primary.DefaultClip = 30
 SWEP.Primary.Automatic = true
 SWEP.Primary.Ammo = "smg1"
 
